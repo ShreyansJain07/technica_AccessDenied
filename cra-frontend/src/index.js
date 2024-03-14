f241d0bc8c29cf7c38e15c43f7ce2a29d0d21e50
Ritojnan <ritojnanm@gmail.com>
2024-03-14 19:24:57 +0530

    mouse pointer corrected

--- cra-frontend/src/index.js
+++ cra-frontend/src/index.js
@@ -5,18 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import MouseContextProvider from "./context/mouse-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <MouseContextProvider>
     <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
-    </MouseContextProvider> 
   </React.StrictMode>
 );
 
--- cra-frontend/src/layouts/RootLayout.jsx
+++ cra-frontend/src/layouts/RootLayout.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import VoiceButton from "../components/VoiceButton";
 import SpeechSynthesizer from "../components/SpeechSynthesizer";
-import CustomCursor from "../components/CustomCursor";
 
 import { useContext } from "react";
-import { MouseContext } from "../context/mouse-context";
 
 
 import LandingTop from "../components/LandingTop";
 export default function RootLayout() {
-  const { cursorType, cursorChangeHandler } = useContext(MouseContext);
 
   return (
     <>
@@ -18,11 +15,8 @@ export default function RootLayout() {
       <FeatureList/> */}
       <SpeechSynthesizer/>
       <div className="App">
-      <CustomCursor />
       <div className="container">
         <div
-          onMouseEnter={() => cursorChangeHandler("hovered")}
-          onMouseLeave={() => cursorChangeHandler("")}
         >
           <h1>Hover over me</h1>
         </div>
