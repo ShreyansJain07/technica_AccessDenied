0a9b6dc2c5c52832c29a74644a08d4fb7c11f4c1
Varun Jajoo <128844637+Varun-Jajoo@users.noreply.github.com>
2024-03-15 02:11:53 +0530

    serp api

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const port = 8000; // Choose the port you want to run the server on
+const port = 8000; 
 app.use(
   cors({
     origin: "*",
