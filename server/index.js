539c6b7a82be25103fd49499cfc8607f48d6ace7
Shreyans jain <shreyansjain1603@gmail.com>
2024-03-15 11:32:53 +0530

    job seek completed

--- server/index.js
+++ server/index.js
@@ -20,23 +20,9 @@ app.get("/jobs", async (req, res) => {
     console.log(req.query);
     const  query  = req.query.query.replace(/ /g,"+");
     console.log(query);
-    // const query = "full stack developer mumbai";
-
-    // const serpApiUrl = "https://serpapi.com/search";
-    // const response = await axios.get(serpApiUrl, {
-    //   params: {
-    //     api_key:
-    //       "98a1671d5b1c1efae84bf3516820fab33875cb7a4e541c9fa4ddc6523eadeb98",
-    //     engine: "google_jobs",
-    //     lrad: "0",
-    //     q: query,
-    //     google_domain: "google.com",
-    //     gl: "in",
-    //   },
-    // });
 
     const response = await axios.get(
-      `https://serpapi.com/search.json?engine=google_jobs&q=${query}&google_domain=google.com&gl=in&hl=en&api_key=98a1671d5b1c1efae84bf3516820fab33875cb7a4e541c9fa4ddc6523eadeb98`
+      `https://serpapi.com/search.json?engine=google_jobs&q=${query}&google_domain=google.com&gl=in&hl=en&api_key=5d1b79b4ee809fa8365d09f2d36a866dda3b7e1e2f93ad1c59726eff1454f4f2`
     );
 
     res.json(response.data);
